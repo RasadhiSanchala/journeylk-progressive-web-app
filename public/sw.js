d7e5e685ae24939965d5879869a293a0d7e438eb
Rasadhi Sanchala <rasadhi.sanchala@atlato.com>
2026-06-20 21:43:09 +0530

    fix(pwa): add correctly sized app icons

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,10 @@
 const CACHE_NAME = "journeylk-static-v2";
-const STATIC_ASSETS = ["/", "/manifest.json", "/icons/icon-192.svg", "/icons/icon-512.svg"];
+const STATIC_ASSETS = [
+  "/",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS)));
